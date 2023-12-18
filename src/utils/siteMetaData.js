1a1b488855f2509e32c7c5dcabd8ef51005fae8f
han6054 <424681370@qq.com>
2023-12-18 22:27:14 +0800

    feat: readme

---
+++ src/utils/siteMetaData.js
@@ -0,0 +1,22 @@
+// please update required information
+const siteMetadata = {
+  title: "Next.js Blog With Tailwind CSS and Contentlayer",
+  author: "jaylan",
+  headerTitle: "Next.js Blog",
+  description: "A blog created with Next.js, Tailwind.css and contentlayer.",
+  language: "en-us",
+  theme: "system", // system, dark or light
+  siteUrl: "", // your website URL
+  siteLogo: "/logo.png",
+  socialBanner: "/social-banner.png", // add social banner in the public folder
+  email: "hanjiajun6054@gmail.com",
+  github: "https://github.com/han6054",
+  twitter: "https://twitter.com/hanjiajun6054",
+  facebook: "https://facebook.com",
+  youtube: "https://youtube.com/",
+  linkedin: "https://www.linkedin.com/in/jiajun-han-a0197a29b/",
+  // dribbble: "https://www.dribbble.com",
+  locale: "en-US",
+};
+
+module.exports = siteMetadata;
