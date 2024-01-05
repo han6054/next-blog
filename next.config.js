ab206fbcd21ffd4aabffacebc6a9dd1978bfcbfc
han6054 <424681370@qq.com>
2024-01-06 00:18:41 +0800

    next-transpile-modules

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const { withContentlayer } = require("next-contentlayer");
+const withTM = require("next-transpile-modules")(["three"]);
 
 const nextConfig = {
   compiler: {
@@ -8,4 +9,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withContentlayer({ ...nextConfig });
+module.exports = withContentlayer({ ...nextConfig, ...withTM });
